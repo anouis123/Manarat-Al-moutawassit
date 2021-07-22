506e00429cd25e12c6fea80cb6a669117e4bf927
anouis123 <56122929+anouis123@users.noreply.github.com>
2021-07-22 21:57:18 +0100

    first commit

---
+++ market/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Stourism.db'
+app.config['SECRET_KEY'] = '35ec4695e695d5c8c77e7c22'
+db = SQLAlchemy(app)
+
+from market import routes
+
