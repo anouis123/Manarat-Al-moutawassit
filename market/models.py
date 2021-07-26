505fdc89752b6a611332e1d1c5828325320bd9a5
anouis123 <56122929+anouis123@users.noreply.github.com>
2021-07-26 11:25:24 +0100

    registration complete

--- market/models.py
+++ market/models.py
@@ -1,5 +1,6 @@
 from market import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -14,9 +15,8 @@ class Item(db.Model):
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-    #description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name}'
-
