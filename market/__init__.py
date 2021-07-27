1eb5cfdef1ca6053cb2237dbf873e053aa9ad350
anouis123 <56122929+anouis123@users.noreply.github.com>
2021-07-27 14:55:52 +0100

    auth1 : encrypting password

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Stourism.db'
 app.config['SECRET_KEY'] = '35ec4695e695d5c8c77e7c22'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from market import routes
 
